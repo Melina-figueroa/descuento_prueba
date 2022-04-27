a630f75a0e88724bf9a74abd77cb80732e14bdb8
Melina Figueroa <104174303+Melina-figueroa@users.noreply.github.com>
2022-04-27 12:00:54 -0300

    prueba descuento

--- src/descuento.ts
+++ src/descuento.ts
@@ -1,5 +1,5 @@
-let precioProducto: number = Number(prompt("ingresePrecio"));
-let porcentajeDescuento: number = Number(0.1);
+let precioProducto: number = Number(prompt("ingresePrecio")); //este es el valor del producto
+let porcentajeDescuento: number = Number(0.1); //desceunto a aplicar
 let descuento: number = Number(precioProducto * porcentajeDescuento);
 let precioFinal: number = precioProducto - descuento;
 console.log(precioFinal);
